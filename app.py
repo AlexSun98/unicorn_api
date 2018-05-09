1ab1e803e1ec62529ccaa7f0aeb9987c6d5723e5
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-05-09 16:18:38 +0000

    It Compiles! 50 Points For Gryffindor.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'a8e25947d4581fa3d5bd0d9ff5921731'
-__version__ = '98ede9714971b6cec54dcd04039ab160'
+__vcs_id__ = '154fe544db9511a8660a36e9667ac4a6'
+__version__ = '9dfd1adbc651b80f3b13f0f07febeaa8'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
