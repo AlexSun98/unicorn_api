c2440768f16e4d99ee4ebb484cd5f400fb5777a4
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-05-09 21:18:34 +0000

    Try feeding a unicorn a SPARKLE_BERRY

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'e120b41e717294d8f58ca7f4c3653edb'
-__version__ = '7eafd5dd16ffadf9939cb4577fe1a16d'
+__vcs_id__ = '1182f5d5f4e962be135305d8c0ff1de5'
+__version__ = 'c84fad077daa44a01836d21c150eeedf'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
