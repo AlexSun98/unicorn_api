e0886c2204bc652aa12220c591bcacad0768cfcc
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-05-09 07:55:56 +0000

    I fixed the deal with that thing

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '9f5af51d64577239be49c61e226d0081'
-__version__ = '4f6a5dc29d11791a689291dda005a5b0'
+__vcs_id__ = '9f0e3824015d6ed4c2047a2f823e60c1'
+__version__ = '46f609eddcea509bf3b97b14c9efb936'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
