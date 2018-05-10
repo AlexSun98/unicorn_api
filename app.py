bfadd2d4dec43d353667e57cdbffec67e64ba3a0
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-05-10 21:33:32 +0000

    Fix PC Load Letter Error

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '90837b9a982ac2afd3a685506d134f32'
-__version__ = '5cfc12b23b77725567d9ae2c9ffd23cb'
+__vcs_id__ = '86c9e64fe1bf121e4af9ecace6fe9618'
+__version__ = '5d98a60434725118891ce537cd2a1fc8'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
