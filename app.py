78205f2b66c9349433db02c7236db14686469e4d
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-05-11 01:19:06 +0000

    Fix the fixes

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'de28115d13187114ba1a6106fa060f4f'
-__version__ = '89edc4d5f4811aa5586c985e679eef0a'
+__vcs_id__ = 'f51e0a68123f8ab87d53ec097a2fd5f6'
+__version__ = '53d4c3f124b1d493eab8f2eda8cf5cc2'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
